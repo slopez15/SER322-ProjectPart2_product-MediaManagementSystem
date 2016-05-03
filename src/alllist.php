d017dbe878796dcab205ac29cfd3c2cb3cad8310
mazarag2 <mazarag2@asu.edu>
2016-05-02 19:35:24 -0700

    Updated Php file

--- src/alllist.php
+++ src/alllist.php
@@ -1,3 +1,37 @@
+
 <?php
-	//past code here
+
+$host = '127.0.0.1:3306';
+$user = 'root';
+$password = 'pleaseconnect123';
+$dbName = 'shoppingcart12';
+$port = 3306;
+
+$conn = new mysqli($host,$user,$password,$dbName);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+displayLibrary($conn);
+
+$conn->close();
+
+function displayLibrary($conn){
+	$sql = "SELECT * FROM digitallibrary";
+
+	$result = $conn->query($sql);
+
+	if($result->num_rows > 0){
+		while($row = $result->fetch_assoc()){
+			echo "MediaID " . $row["MediaID"] . "ISBN " . $row["ISBN"] .
+			"Title " . $row["Title"] . 
+			  "<br>";
+			
+		}
+	}
+}
+
+
 ?>
+
