b73e693e41eb984b1d7a80383685e95061f88496
Saul Lopez <slopez15@asu.edu>
2016-05-04 16:54:13 -0700

    Can now see all products in library and deletes the product by UFC and not all products of same isbn

--- src/musiclist.php
+++ src/musiclist.php
@@ -1,9 +1,69 @@
 <?php
+/**
+*Deletes a digitalMedia from the Library using
+*ISBN as a key
+**/
+
 require("mysqli_connect.php");
 
-displayVideo($conn);
+handleEvent(displayVideo($conn));
 
 $conn->close();
+
+
+function displayVideo($conn){
+	//prints all copies of music type, not print if not in library and  mysql not add into library unless there is a mediadescription for it.
+	$sql = "SELECT * 
+		FROM mediadescription, digitallibrary 
+		WHERE Type = 'Music' AND mediadescription.ISBN=digitallibrary.ISBN";
+	$result = $conn->query($sql);
+
+	$counter = 0;
+	if($result->num_rows > 0){
+		?>
+		<form action = "<?php echo $_SERVER['PHP_SELF'];?>" method = "POST">
+		<?php
+		while($row = $result->fetch_assoc()){
+			echo "<br/>";
+			echo "<table border=1>
+			<tr>
+				<th>UFC</th>
+				<th>ISBN</th>
+				<th>Title</th>
+				<th>Type</th>
+				<th>Category</th>
+				<th>Year</th>
+				<th>Author</th>
+				<th>Cost</th>
+			</tr>";
+			?>
+			
+			<input type="checkbox" name="musiclist[]" value="<?php echo $row['UFC'];?>">Add To Cart</input>
+			</form>
+
+			<?php
+			echo "<tr>";
+				echo "<td>" . $row['UFC'] . "</td>";
+				echo "<td>" . $row['ISBN'] . "</td>";
+				global $isbnArr;
+				$isbnArr[$counter] = $row['UFC'];
+				echo "<td>" . $row['Title'] . "</td>";
+				echo "<td>" . $row['Type'] . "</td>";
+				echo "<td>" . $row['Category'] . "</td>";
+				echo "<td>" . $row['Year'] . "</td>";
+				echo "<td>" . $row['Author'] . "</td>";
+				echo "<td>" . $row['Cost'] . "</td>";
+			echo "</tr>";
+			?>
+			<?php
+			echo "</table>";
+			++$counter;
+		}
+	}
+	//used to be isbnArr
+	//creative here this the problem
+	return $isbnArr;
+}
 
 //global variable
 function handleEvent($arr){
@@ -42,73 +102,15 @@ function handleEvent($arr){
 		}
 	}
 }
-/**
-*Deletes a digitalMedia from the Library using
-*ISBN as a key 
-**/
 
-function DelQuery($ISBN){
 
-	$host = '127.0.0.1:3306';
-	$user = 'root';
-	$password = 'pleaseconnect123';
-	$dbName = 'shoppingcart12';
-	$port = 3306;
+function DelQuery($UFC){
+	require("mysqli_connect.php");
 
-	$conn = new mysqli($host,$user,$password,$dbName);
-
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	} 
-	$query = "DELETE FROM digitallibrary WHERE ISBN = $ISBN";
+	$query = "DELETE FROM digitallibrary WHERE UFC = $UFC";
 	$result = $conn->query($query);
 	$conn->close();
 }
 
-function displayVideo($conn){
-	$sql = "SELECT * FROM mediadescription WHERE Type = 'Music'";//changed from digitalLibrary
-	$result = $conn->query($sql);
-	$counter = 0;
-	if($result->num_rows > 0){
-		?>
-		<form action = "<?php echo $_SERVER['PHP_SELF'];?>" method = "POST">
-		<?php
-		while($row = $result->fetch_assoc()){
-			echo "<br>";
-			echo "<table border=1>
-			<tr>
-			<th>ISBN</th>
-			<th>Title</th>
-			<th>Type</th>
-			<th>Category</th>
-			<th>Year</th>
-			<th>Author</th>
-			<th>Cost</th>
-			</tr>";
-			?>
-			
-			<input type = "checkbox" name = "musiclist[]" value = "<?php echo $row['ISBN'];?>">Add To Cart</input>
-			</form>
-			<?php
-			echo "<tr>";
-			echo "<td>" . $row['ISBN'] . "</td>";
-			global $isbnArr;
-			$isbnArr[$counter] = $row['ISBN'];
-			echo "<td>" . $row['Title'] . "</td>";
-			echo "<td>" . $row['Type'] . "</td>";
-			echo "<td>" . $row['Category'] . "</td>";
-			echo "<td>" . $row['Year'] . "</td>";
-			echo "<td>" . $row['Author'] . "</td>";
-			echo "<td>" . $row['Cost'] . "</td>";
-			echo "</tr>";
-			?>
-			<?php
-			echo "</table>";
-			++$counter;
-		}
-	}
-	//used to be isbnArr
-	//creative here this the problem
-	return $isbnArr;
-}
+
 ?>
